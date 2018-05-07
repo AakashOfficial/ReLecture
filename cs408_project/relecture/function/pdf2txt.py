4070536d86acb03c297c23332f946d0e96036284
HaSeongKwon <>
2018-05-07 21:36:16 +0900

    pdf upload, pdf view 수정

--- cs408_project/relecture/function/pdf2txt.py
+++ cs408_project/relecture/function/pdf2txt.py
@@ -1,9 +1,13 @@
 import PyPDF2
 import json
 import csv
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def pdf2txt(pdf, output_json, output_csv):
-	
+	path = os.path.join(BASE_DIR, 'convert_pdf')
+
 	#read pdf
 	pdfFileObj = open(pdf,'rb')
 	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
@@ -15,16 +19,16 @@ def pdf2txt(pdf, output_json, output_csv):
 
 	#save as json file
 	json_val=json.dumps(slide_by_number)
-	f = open(output_json, "w")
+	f = open(os.path.join(path, output_json), "w")
 	f.write(json_val)
 	f.close()
 
 	#save as csv file
-	w = csv.writer(open(output_csv, "w"))
+	w = csv.writer(open(os.path.join(path, output_csv), "w"))
 	for key, val in slide_by_number.items():
 		w.writerow([key, val])
 
 	return True
 
-# example : pdf2txt('UML.pdf', '1.json', '2.csv')
+#example : pdf2txt('UML.pdf', '1.json', '2.csv')
 # pdf2txt('UML.pdf', '1.json', '2.csv')
